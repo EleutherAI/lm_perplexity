b6bafa90b43c8040a21582034293795b4cb9482e
Jason Phang <email@jasonphang.com>
2020-11-26 18:52:43 -0500

    new_rolling_window

--- lm_perplexity/utils.py
+++ lm_perplexity/utils.py
@@ -12,23 +12,45 @@ def write_json(data, path, **kwargs):
         f.write(json.dumps(data))
 
 
-def get_rolling_token_windows(token_list, max_seq_len, context_len):
-    pred_len = max_seq_len - context_len
-    rolling_windows_and_pred_start = []
+def get_rolling_token_windows(token_list, prefix_token, max_seq_len, context_len):
+    """
+    - context_len allows for a rolling window context, allowing each prediction window to potentially
+      condition on some context
+
+    :param token_list: list
+        List of tokens to be PREDICTED
+    :param max_seq_len: int
+        max_seq_len of model (or max_seq_len we want to use)
+    :param context_len: int
+        Amount of desired token context for prediction. Needs to be at least 1.
+    :param prefix_token: token
+        Dummy token like <eos> so the first token has something to condition on
+    :return: generator
+        Generator of tuples
+            (input_tokens, pred_tokens)
+        Note: Score only the last len(pred_tokens) logits of the LM
+    """
+    assert 1 <= context_len <= max_seq_len
+    # +1 offset, going from input->preds
+    pred_len = max_seq_len - context_len + 1
     predicted = 0
 
     # Special handling for first window: predict all tokens
-    first_window = token_list[:max_seq_len]
-    rolling_windows_and_pred_start.append((first_window, 0))
-    predicted += len(first_window)
+    first_seq_len = min(max_seq_len, len(token_list))
+    yield (
+        [prefix_token] + token_list[:first_seq_len - 1],
+        token_list[:first_seq_len]
+    )
+    predicted += first_seq_len
 
     while predicted < len(token_list):
-        window_end = predicted + pred_len
-        window = token_list[window_end - max_seq_len:window_end]
-        rolling_windows_and_pred_start.append((window, context_len))
-        predicted += len(window) - context_len
-
-    return rolling_windows_and_pred_start
+        window_pred_len = min(len(token_list) - predicted, pred_len)
+        window_end = predicted + window_pred_len
+        yield (
+            token_list[window_end - max_seq_len - 1:window_end - 1],
+            token_list[window_end - window_pred_len:window_end],
+        )
+        predicted += window_pred_len
 
 
 class LogprobsPositionBuckets:
